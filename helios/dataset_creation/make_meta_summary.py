3e6aad54ccb4f7aa8e9204bc282cc0d1e0f5e562
Favyen Bastani <favyenb@allenai.org>
2025-01-21 15:40:53 -0800

    add test for make_meta_summary

--- helios/dataset_creation/make_meta_summary.py
+++ helios/dataset_creation/make_meta_summary.py
@@ -5,6 +5,44 @@ import csv
 
 from upath import UPath
 
+from .util import get_modality_temp_meta_dir
+
+
+def make_meta_summary(helios_path: UPath, modality: str) -> None:
+    """Create the concatenated metadata file for the specified modality.
+
+    The data files and per-example temporary metadata files must be populated for the
+    modality already. This function just concatenates those temporary metadata files
+    together into one big CSV.
+
+    Args:
+        helios_path: Helios dataset path.
+        modality: modality to write summary for.
+    """
+    # Concatenate the CSVs while keeping the header only from the first file that we
+    # read.
+    column_names: list[str] | None = None
+    csv_rows = []
+    meta_dir = get_modality_temp_meta_dir(helios_path, modality)
+    for fname in meta_dir.iterdir():
+        with fname.open() as f:
+            reader = csv.DictReader(f)
+            if reader.fieldnames is None:
+                raise ValueError(f"CSV at {fname} does not contain header")
+            if column_names is None:
+                column_names = list(reader.fieldnames)
+            for csv_row in reader:
+                csv_rows.append(csv_row)
+
+    if column_names is None:
+        raise ValueError(f"did not find any files in {meta_dir}")
+
+    with (helios_path / f"{modality}.csv").open("w") as f:
+        writer = csv.DictWriter(f, fieldnames=column_names)
+        writer.writeheader()
+        writer.writerows(csv_rows)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Create concatenated metadata file",
@@ -22,28 +60,4 @@ if __name__ == "__main__":
         required=True,
     )
     args = parser.parse_args()
-
-    helios_path = UPath(args.helios_path)
-
-    # Concatenate the CSVs while keeping the header only from the first file that we
-    # read.
-    column_names: list[str] | None = None
-    csv_rows = []
-    meta_dir = helios_path / f"{args.modality}_meta"
-    for fname in meta_dir.iterdir():
-        with fname.open() as f:
-            reader = csv.DictReader(f)
-            if reader.fieldnames is None:
-                raise ValueError(f"CSV at {fname} does not contain header")
-            if column_names is None:
-                column_names = list(reader.fieldnames)
-            for csv_row in reader:
-                csv_rows.append(csv_row)
-
-    if column_names is None:
-        raise ValueError(f"did not find any files in {meta_dir}")
-
-    with (helios_path / f"{args.modality}.csv").open("w") as f:
-        writer = csv.DictWriter(f, fieldnames=column_names)
-        writer.writeheader()
-        writer.writerows(csv_rows)
+    make_meta_summary(UPath(args.helios_path), args.modality)
