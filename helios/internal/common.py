18c9c0f92fe1bee7f49bcc365dad75ce34a81591
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-05-01 17:37:56 +0800

    Same changes as in #220 so that experiments can run

--- helios/internal/common.py
+++ helios/internal/common.py
@@ -127,7 +127,7 @@ def build_launch_config(
             "conda shell.bash activate base",
             # Install torch==2.7 if we're targetting titan
             "pip install -e '.[all]'",
-            "pip install --upgrade beaker-py",
+            # "pip install --upgrade beaker-py",
             # Quickly try a new version of PyTorch like this
             #  "pip install --upgrade --pre torch==2.6.0.dev20241112+cu121 --index-url https://download.pytorch.org/whl/nightly/cu121",
             pytorch_upgrade,
