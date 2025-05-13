a907615a4c75656d0d1457802a7023bc34d95baa
hgherzog <henryh@allenai.org>
2025-05-13 12:17:24 -0700

    now do no missing

--- scripts/2025_04_23_benchmarking_ladder/base_galileo_max.py
+++ scripts/2025_04_23_benchmarking_ladder/base_galileo_max.py
@@ -194,7 +194,7 @@ def build_dataset_config(common: CommonComponents) -> Config:
         HeliosDatasetConfig(
             h5py_dir="/weka/dfive-default/helios/dataset/presto/h5py_data_w_missing_timesteps_gzip_3/landsat_naip_10_openstreetmap_raster_sentinel1_sentinel2_l2a_srtm_worldcover/117473",
             training_modalities=common.training_modalities,
-            use_modalities_with_missing_timesteps=True,
+            use_modalities_with_missing_timesteps=False,
             dtype=DType.float32,
             # cache_dir="/helios_cache/presto",
             # samples_per_sec=4 / NUM_WORKERS,  # 2/ GBS
@@ -202,7 +202,7 @@ def build_dataset_config(common: CommonComponents) -> Config:
         HeliosDatasetConfig(
             h5py_dir="/weka/dfive-default/helios/dataset/osm_sampling/h5py_data_w_missing_timesteps_gzip_3/landsat_naip_10_openstreetmap_raster_sentinel1_sentinel2_l2a_srtm_worldcover/285288",
             training_modalities=common.training_modalities,
-            use_modalities_with_missing_timesteps=True,
+            use_modalities_with_missing_timesteps=False,
             dtype=DType.float32,
             # cache_dir="/helios_cache/osm_sampling",
             # samples_per_sec=4 / NUM_WORKERS,  # 2/ GBS
