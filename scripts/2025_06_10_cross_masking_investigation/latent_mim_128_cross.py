f750b3e3dd32cb3aad4505b2e992c6a842bdcf10
hgherzog <henryh@allenai.org>
2025-06-12 10:16:10 -0700

    fix rmb

--- scripts/2025_06_10_cross_masking_investigation/latent_mim_128_cross.py
+++ scripts/2025_06_10_cross_masking_investigation/latent_mim_128_cross.py
@@ -91,7 +91,7 @@ def build_train_module_config(
     return LatentMIMTrainModuleConfig(
         optim_config=AdamWConfig(lr=0.0001, weight_decay=0.02),
         warmup_duration=Duration.steps(8000),
-        rank_microbatch_size=256,  # Can be 256 on titan, needs to be <= 64 (i think) on jupiter
+        rank_microbatch_size=64,  # Can be 256 on titan, needs to be <= 64 (i think) on jupiter
         masking_config=MaskingConfig(
             strategy_config={
                 "type": "modality_cross_space_time",
