dfc3f4c1c2d896bc974b9b800b9241a99efbcaed
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-10-07 13:08:35 +0100

    modality ablations, random projection

--- scripts/2025_10_02_phase2/ablations/base_launch_ablations.sh
+++ scripts/2025_10_02_phase2/ablations/base_launch_ablations.sh
@@ -4,9 +4,18 @@ python scripts/2025_10_02_phase2/base.py launch phase2.0_base_no_contrastive ai2
 python scripts/2025_10_02_phase2/base.py launch phase2.0_base_random_masking ai2/ceres-cirrascale  --train_module.masking_config.strategy_config="{'type': 'random', 'encode_ratio': 0.5, 'decode_ratio': 0.5}" --launch.clusters='[ai2/jupiter-cirrascale-2]' --launch.priority=high
 # MAE
 python scripts/2025_10_02_phase2/ablations/base_mae.py phase2.0_base_mae ai2/ceres-cirrascale  --launch.clusters='[ai2/jupiter-cirrascale-2]' --launch.priority=high
-# no ag maps (removing worldcereal and CDL)
-python scripts/2025_10_02_phase2/ablations/no_ag_maps.py phase2.0_base_mae ai2/ceres-cirrascale  --launch.clusters='[ai2/jupiter-cirrascale-2]' --launch.priority=high
-# no maps (removing ag maps + worldcover, openstreetmap, canopy height)
-python scripts/2025_10_02_phase2/ablations/no_maps.py phase2.0_base_mae ai2/ceres-cirrascale  --launch.clusters='[ai2/jupiter-cirrascale-2]' --launch.priority=high
 # random init the target projections
 python scripts/2025_10_02_phase2/base.py launch phase2.0_base_random_target ai2/ceres-cirrascale  --train_module.reinit_targets=True --launch.clusters='[ai2/jupiter-cirrascale-2]' --launch.priority=high
+###### Modality abations below ######
+# I have successively removed modalities here, so that
+# each run has fewer modalities than a previous one
+# no ag maps (removing worldcereal and CDL)
+python scripts/2025_10_02_phase2/ablations/base.py phase2.0_base_mae ai2/ceres-cirrascale  --launch.clusters='[ai2/jupiter-cirrascale-2]' --launch.priority=high --common.training_modalities='[sentinel2_l2a,sentinel1,landsat,worldcover,srtm,openstreetmap_raster,wri_canopy_height_map]' --train_module.masking_config.strategy_config.only_decode_modalities='[worldcover,srtm,openstreetmap_raster,wri_canopy_height_map]'
+# no maps (removing ag maps + worldcover, openstreetmap, canopy height)
+python scripts/2025_10_02_phase2/ablations/base.py phase2.0_base_mae ai2/ceres-cirrascale  --launch.clusters='[ai2/jupiter-cirrascale-2]' --launch.priority=high --common.training_modalities='[sentinel2_l2a,sentinel1,landsat,srtm]' --train_module.masking_config.strategy_config.only_decode_modalities='[srtm]'
+# no decode modalities (removing maps + srtm)
+python scripts/2025_10_02_phase2/ablations/base.py phase2.0_base_mae ai2/ceres-cirrascale  --launch.clusters='[ai2/jupiter-cirrascale-2]' --launch.priority=high --common.training_modalities='[sentinel2_l2a,sentinel1,landsat]' --train_module.masking_config.strategy_config.only_decode_modalities='[]'
+# no landsat
+python scripts/2025_10_02_phase2/ablations/base.py phase2.0_base_mae ai2/ceres-cirrascale  --launch.clusters='[ai2/jupiter-cirrascale-2]' --launch.priority=high --common.training_modalities='[sentinel2_l2a,sentinel1]' --train_module.masking_config.strategy_config.only_decode_modalities='[]'
+# s2 only (no s1)
+python scripts/2025_10_02_phase2/ablations/base.py phase2.0_base_mae ai2/ceres-cirrascale  --launch.clusters='[ai2/jupiter-cirrascale-2]' --launch.priority=high --common.training_modalities='[sentinel2_l2a]' --train_module.masking_config.strategy_config.only_decode_modalities='[]'
